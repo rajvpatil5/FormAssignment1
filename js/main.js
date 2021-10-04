d6f048121edcc1e2d0a00fba5cdd1bd8f42a8798
rajvpatil5 <rajvpatil5@gmail.com>
2021-10-04 22:47:21 +0530

    form created using bootstrap form classes

---
+++ js/main.js
@@ -0,0 +1,106 @@
+/**********main.js*********/
+//var abc = 2345;
+/*var abc = "test scope";
+
+console.log(abc)
+
+var abc = "value changed";
+
+console.log(abc);
+
+var abc = "3rd Step overrite";
+
+console.log(abc);*/
+
+/*let abc = "test scope";
+
+console.log(abc)
+
+abc = "value changed";
+
+console.log(abc);*/
+
+/*const abc = "this is const one";
+console.log(abc);
+
+abc= "duplicate const";
+console.log(abc);*/
+
+/*var testArr = [];
+
+function f1(){testarr = 12344}
+function f2(){testArr = 2345}
+
+console.log(f2())
+
+srajat.patil@gmail.com
+
+
+skill.swapnil@gmail.com
+*/
+
+//Data Type In JS
+
+/*Primitive:
+Number, String, Boolean, Null and Undefined*/
+
+var abc;
+
+var xyz = "";
+
+//var dsr = true / false;
+
+//var / let ytr = null;
+
+var unde;
+
+/*typof *****/
+
+/*Non Primitive:
+Object, Array */
+
+//Object
+
+var obj = {}; //blank object
+
+//let obj1 = {};
+
+//const obj2 = {};
+
+obj = {
+  id: 123,
+  name: "Suhas",
+  selected: true,
+};
+//left side value consider as a key
+// right hand side value consider as value
+//based on specified method you can iterate obj or its value
+
+var arr1 = [123, "Suhas name", false]; //blank array object
+
+//variable scope and overritten rules implemented as per there defination. Like var, let and const
+
+//Array object consider combination of index and value
+//Array object having different propertie with length
+//based on length you can iterate every value of array object
+
+//Shallow copy
+//object copy within another object and change value from another object then original object value should be affected.
+
+//Deep Copy
+//Original object copy into another. When another object will change value then original object value should not affected.
+// spread operator will define role of deep copy (syntax: ...)
+// Object.Assign(objname) for deep copy
+
+//Assignment Object and array predefined method and its use.
+//Assignment html form controls should be as below:
+/*
+	Name: text box
+	Address: Text Area
+	Phone no: text box
+	Male / Female : Radio Button
+	Is Working : Checkbox Yes / No
+	Submit and Cancel button
+	write method should show console.log()
+	onClick="console.log()"
+*/
